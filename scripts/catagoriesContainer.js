da4cb6d805c8a64dd3dccf6ff62290788e830529
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-04 22:40:41 +0600

    Plant catagory added

---
+++ scripts/catagoriesContainer.js
@@ -0,0 +1,14 @@
+const catagoriesContainer = document.getElementById("catagoriesContainer");
+async function loadCatagories() {
+  const res = await fetch("../jsonFiles/category.json");
+  const data = await res.json();
+  // console.log(data);
+  data.categories.forEach((catagory) => {
+    console.log(catagory);
+    const btn = document.createElement("button");
+    btn.innerText = catagory.category_name;
+    btn.className = "btn btn-outline w-full";
+    catagoriesContainer.append(btn);
+  });
+}
+loadCatagories();
